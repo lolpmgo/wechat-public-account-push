71de7c89defe034597bade9c2fcf38f76252a513
lolpmgo <56176402+lolpmgo@users.noreply.github.com>
2022-11-27 23:58:02 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oSk5C58eK0n9o3jDFUQpqyehS_yA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'seegCVEiA4DKDwny2y4u_VQkT3iNWtt5YT3hsUfTlpo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-13',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {

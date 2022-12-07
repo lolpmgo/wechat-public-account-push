499b64f73ad4e0da938785e1f4012960de22a182
lolpmgo <56176402+lolpmgo@users.noreply.github.com>
2022-12-06 23:01:22 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oSk5C58eK0n9o3jDFUQpqyehS_yA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'seegCVEiA4DKDwny2y4u_VQkT3iNWtt5YT3hsUfTlpo',
+      useTemplateId: 'hD18BGJ0vUWmynchkh2OoclOfdgKXjINQm6eCHb6fVc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-13',
       festivals: [
